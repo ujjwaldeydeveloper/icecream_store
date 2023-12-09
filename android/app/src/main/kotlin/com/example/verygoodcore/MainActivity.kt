19ccfadd22004350a597edbb99f2fe5900881e26
Ujjwal <ujjwal@Ujjwals-MacBook-Pro.local>
2023-12-09 19:21:01 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.icecream_store
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:icecream_store/app/app.dart';
+import 'package:icecream_store/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
